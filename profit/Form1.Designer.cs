7b05446b489bbcf0013c2f16ba79059ba435ed20
madmail <rasulsultanshin@gmail.com>
2022-02-24 02:48:52 +0500

    BGU

---
+++ profit/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace profit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(122, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(122, 44);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(57, 22);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(16, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Сумма (руб.):";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(16, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Срок (мес.):";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(16, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(316, 64);
+            this.label3.TabIndex = 4;
+            this.label3.Click += new System.EventHandler(this.label3_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Расчёт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.butten1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(347, 168);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Доход";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ profit/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace profit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label3.Text = ""; // очистить поле отображения результата расчета
+            if (textBox1.Text.Length == 0 || textBox2.Text.Length == 0)
+                // В поле редактирования нет данных.
+                // Сделать кнопку Расчёт недоступной
+                button1.Enabled = false;
+            else
+                // Сделать кнопку Расчёт доступной
+                button1.Enabled = true;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void butten1_Click(object sender, EventArgs e)
+        {
+            int period; // срок
+            double sum, percent, profit; // сумма, процентная ставка, доход
+            try
+            {
+                sum = System.Convert.ToDouble(textBox1.Text);
+                period = System.Convert.ToInt32(textBox2.Text);
+                if (sum < 10000)
+                    percent = 8.5;
+                else
+                    percent = 12.0;
+                profit = sum * (percent / 100.0 / 12.0) * period;
+                label3.Text = "Процентная ставка: " + percent.ToString("n") + "%\n" +
+                "Доход: " + profit.ToString("c");
+            }
+            catch (FormatException exc)
+            {
+                if (textBox1.Text.Length == 0 || textBox2.Text.Length == 0)
+                    MessageBox.Show("Оба поля должны быть заполнены",
+                    "Доход", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else
+                    MessageBox.Show("Ошибка в исходных данных.\n" +
+                    "В поле Сумма надо ввести целое или дробное число\n" +
+                    "(в качестве десятичного разделителя используйте " +
+                    "запятую),\nв поле Срок - целое.", "Доход",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= '0' && e.KeyChar <= '9') // цифра
+                return;
+            // "Правильный" десятичный разделитель — запятая.
+            // Заменим точку запятой
+            if (e.KeyChar == '.')
+                e.KeyChar = ',';
+            if (e.KeyChar == ',')
+            {
+                // Нажата запятая. Проверим,
+                // может, запятая уже есть в поле редактирования
+                if (textBox1.Text.IndexOf(',') != -1 || textBox1.Text.Length == 0)
+                    // Запятая уже есть.
+                    // Запретить ввод еще одной
+                    e.Handled = true;
+                return;
+            }
+            if (Char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (char)Keys.Enter)
+                    // Нажата клавиша <Enter>.
+                    // Переместить курсор в поле Срок
+                    textBox2.Focus();
+                return;
+            }
+            //Остальные символы запрещены
+            e.Handled = true;
+
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //в поле Срок можно ввести только целое число
+            if (e.KeyChar >= '0' && e.KeyChar <= '9') // цифра
+                return;
+            if (Char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (char)Keys.Enter)
+                    // Нажата клавиша <Enter>.
+                    // Переместить курсор на кнопку Расчёт
+                    button1.Focus();
+                return;
+            }
+            //остальные символы запрещены
+            e.Handled = true;
+        }
+        // Эта функция обрабатывает событие TextChanged (изменился текст в поле
+        // редактирования) обоих компонентов TextBox.
+        // Сначала надо обычным образом создать функцию обработки события
+        // TextChanged для компонента textBox1, а затем указать ее в качестве
+        // обработчика события TextChanged для компонента textBox2
+    }
+}
